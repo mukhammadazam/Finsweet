1e491e377382d1f91cfa83d97b4190621363134a
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-09 00:46:01 +0300

    api fetch

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
 const navList = document.querySelector('.nav__list');
 const burger = document.querySelector(".nav__burger");
 const navbar = document.querySelector(".nav");
+const row = document.querySelector('.row');
+const myKey = 'a5bf6f3b316846b689ef768df6effaad';
+// MODAL UCHUN FUNKSIYA
 burger.addEventListener('click', () => {
     navList.classList.toggle('nav__list1');
     // navbar.classList.toggle('navbar')
 })
+// API FETCH QILISH UCHUN FUNKSIYA
+const url = `https://newsapi.org/v2/everything?q=tesla&from=2023-02-08&sortBy=publishedAt&apiKey=${myKey}`
+async function ascFun() {
+    const res = await fetch(url);
+    const data = await res.json();
+    try {
+        data.articles.map((Element) => {
+            console.log(Element);
+            const card = document.createElement('div');
+            const img = document.createElement('img');
+            const cardBody = document.createElement('div');
+            const title = document.createElement('h5');
+            const dataTime = document.createElement('div');
+            card.classList.add('card');
+            cardBody.classList.add('card-body');
+            title.classList.add('card-title');
+            img.src = Element.urlToImage;
+            dataTime.innerHTML = `<input class='border-0 ' type='date'/>`
+            title.textContent = Element.author;
+
+            cardBody.appendChild(title);
+            card.append(img, dataTime, cardBody);
+            row.appendChild(card);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+ascFun()
