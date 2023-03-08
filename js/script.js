0f1aebf7aedc3e7edd6059dac5a9ed3edfef4fa5
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-08 20:22:32 +0300

    hero start

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const navList = document.querySelector('.nav__list');
 const burger = document.querySelector(".nav__burger");
+const navbar = document.querySelector(".nav");
 burger.addEventListener('click', () => {
     navList.classList.toggle('nav__list1');
+    // navbar.classList.toggle('navbar')
 })
