f26ba7701419aa8754f03a9be49e45b6ee825e3a
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-09 23:19:13 +0300

    card finished

--- js/script.js
+++ js/script.js
@@ -13,32 +13,34 @@ const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${my
 async function ascFun() {
     const res = await fetch(url);
     const data = await res.json();
-    // data = date.slice(1, 3)
+    console.log(data.articles);
     console.log(data.articles.slice(1, 3));
     try {
 
         data.articles.slice(0, 3).map((Element, index) => {
-                console.log(index);
-                
-                const card = document.createElement('div');
+            console.log(index);
+
+            const card = document.createElement('div');
             const img = document.createElement('img');
             const cardBody = document.createElement('div');
             const title = document.createElement('h5');
+            const title1 = document.createElement('h6');
             const dataTime = document.createElement('div');
-            card.classList.add('card');
+            card.classList.add('card', 'border-0');
             cardBody.classList.add('card-body');
             title.classList.add('card-title');
             img.src = Element.urlToImage;
             dataTime.innerHTML = `<input class='border-0 ' type='date'/>`
             title.textContent = Element.author;
-
+            title1.textContent = Element.content
+            console.log(Element.content);
             cardBody.appendChild(title);
-            card.append(img, dataTime, cardBody);
+            card.append(img, dataTime, cardBody, title1);
             row.appendChild(card);
 
-          
 
-            
+
+
         })
     } catch (error) {
         console.error(error);
