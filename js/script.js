aa87f83642f7352bd483d5e327045d1faa4cd94f
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-10 21:44:39 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -16,26 +16,31 @@ async function ascFun() {
     console.log(data.articles);
     console.log(data.articles.slice(1, 3));
     try {
-
+        console.log(data.articles);
         data.articles.slice(0, 3).map((Element, index) => {
             console.log(index);
 
             const card = document.createElement('div');
             const img = document.createElement('img');
             const cardBody = document.createElement('div');
+            const wrapper = document.createElement('div');
             const title = document.createElement('h5');
             const title1 = document.createElement('h6');
+            const text = document.createElement('p');
             const dataTime = document.createElement('div');
-            card.classList.add('card', 'border-0');
+            card.classList.add('card', 'border-0',);
             cardBody.classList.add('card-body');
-            title.classList.add('card-title');
+            title1.classList.add('card-title', 'text-center');
+            text.classList.add('text-center')
+            wrapper.classList.add('d-flex', 'align-items-start', 'gap-4', 'pt-2', 'justify-content-center')
             img.src = Element.urlToImage;
-            dataTime.innerHTML = `<input class='border-0 ' type='date'/>`
+            dataTime.innerHTML = `<input class='border-0' type='date'/>`
             title.textContent = Element.author;
             title1.textContent = Element.content
-            console.log(Element.content);
+            text.textContent = Element.description
             cardBody.appendChild(title);
-            card.append(img, dataTime, cardBody, title1);
+            wrapper.append(dataTime, title);
+            card.append(img, wrapper, cardBody, title1, text);
             row.appendChild(card);
 
 
