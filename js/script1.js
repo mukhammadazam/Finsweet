53fa2c65aa23451bb12bb6b18a6eb819e98f6e0e
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-14 23:59:28 +0300

    maket finish

--- js/script1.js
+++ js/script1.js
@@ -11,41 +11,54 @@ async function ascFun1() {
         url1 = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${myKey}`
 
     }
-
+    try {
     const row1 = document.querySelector('.row1');
     const respone = await fetch(url1);
     const data = await respone.json();
-    try {
-        row1.innerHTML='';
-        data.articles.map((blogpeg) => {
-            console.log(data.articles[0].content
-                );
-            const card1 = document.createElement('div');
-            const img1 = document.createElement('img');
-            const card1Body = document.createElement('div');
-            const bodyLeni = document.createElement('span');
-            const bodyTitle = document.createElement('h2');
-            const bodtText = document.createElement('p');
-            card1.classList.add('d-lg-flex', 'align-items-lg-center', 'justify-content-lg-between', 'p-sm-5');
-            card1Body.classList.add('ps-lg-5')
-            bodyLeni.classList.add('text-uppercase', 'text-primary', 'd-block', 'pb-3', 'pt-2', 'text-center', 'text-sm-start');
-            bodyTitle.classList.add('bodytitle', 'width-50', 'pb-5', 'fs-2', 'text-center', 'text-sm-start');
-            bodtText.classList.add('bodytext', 'w-sm-75', 'text-center', 'mx-0', 'text-sm-start');
-            img1.classList.add('p-5', 'p-sm-0')
-            img1.src = blogpeg.urlToImage;
-            img1.alt = blogpeg.source.name
-            bodyLeni.textContent = `Business`;
-            bodyTitle.textContent = blogpeg.title.slice(1, 80)
-            bodtText.textContent = blogpeg.description.slice(1, 150)
-            card1Body.append(bodyLeni, bodyTitle, bodtText);
-            card1.append(img1, card1Body);
-            row1.appendChild(card1);
-        })
+    row1.innerHTML = '';
+   if(!data.articles.length){
+    const not = document.createElement('div')
+    not.className='p-5 text-center'
+    not.textContent = 'NOT FOUND'
+    row1.appendChild(not)
+   }else{
+    data.articles.map((blogpeg) => {
+        console.log(data.articles[0].content
+        );
+        const card1 = document.createElement('div');
+        const img1 = document.createElement('img');
+        const card1Body = document.createElement('div');
+        const bodyLeni = document.createElement('span');
+        const bodyTitle = document.createElement('h2');
+        const bodtText = document.createElement('p');
+        card1.classList.add('d-lg-flex', 'align-items-lg-center', 'justify-content-lg-between', 'p-sm-5');
+        card1Body.classList.add('ps-lg-5')
+        bodyLeni.classList.add('text-uppercase', 'text-primary', 'd-block', 'pb-3', 'pt-2', 'text-center', 'text-sm-start');
+        bodyTitle.classList.add('bodytitle', 'width-50', 'pb-5', 'fs-2', 'text-center', 'text-sm-start');
+        bodtText.classList.add('bodytext', 'w-sm-75', 'text-center', 'mx-0', 'text-sm-start');
+        img1.classList.add('p-5', 'p-sm-0')
+        img1.src = blogpeg.urlToImage;
+        img1.alt = blogpeg.source.name
+        bodyLeni.textContent = `Business`;
+        bodyTitle.textContent = blogpeg.title.slice(1, 80)
+        bodtText.textContent = blogpeg.description
+        card1Body.append(bodyLeni, bodyTitle, bodtText);
+        card1.append(img1, card1Body);
+        row1.appendChild(card1);
+    })
+   }
+            
+           
 
-    } catch (error) {
-        console.error(error);
-    }
-}
+        } catch (error) {
+            console.error(error);
+        }
+        
+    } 
+       
+    
+
+
 const input = document.querySelector('.input');
 
 input.addEventListener('keypress', (e) => {
--- js/script2.js
+++ js/script2.js
@@ -8,33 +8,41 @@ async function ascFun2() {
      else {
         url2 = `https://newsapi.org/v2/everything?q=apple&from=2023-03-12&to=2023-03-12&sortBy=popularity&apiKey=${myKey}`
     }
-    const resp = await fetch(url2);
-    const data2 = await resp.json();
     try {
+        const resp = await fetch(url2);
+        const data2 = await resp.json();
         const row3 = document.querySelector('.row2');
         row3.innerHTML = '';
-        data2.articles.map((Element) => {
-            const card2 = document.createElement('div');
-            const img2 = document.createElement('img');
-            const card1Body2 = document.createElement('div');
-            const bodyLeni2 = document.createElement('span');
-            const bodyTitle2 = document.createElement('h2');
-            const bodtText2 = document.createElement('p');
-            card2.classList.add('card2', 'd-lg-flex', 'align-items-center', 'justify-content-lg-between');
-            card1Body2.classList.add('ps-lg-5', 'p-5', 'p-sm-0')
-            img2.classList.add('p-5', 'p-sm-0');
-            bodyTitle2.classList.add('text-center', 'text-sm-start')
-            bodyLeni2.classList.add('d-block', 'text-center', 'text-sm-start','pb-3')
-            bodtText2.classList.add('text-center', 'text-sm-start')
-            img2.src = Element.urlToImage;
-            img2.alt = Element.author;
-            bodtText2.textContent = Element.description.slice(0, 100)
-            bodyTitle2.textContent = Element.title
-            bodyLeni2.textContent = Element.author;
-            card1Body2.append(bodyLeni2, bodyTitle2, bodtText2);
-            card2.append(img2, card1Body2);
-            row3.appendChild(card2)
-        })
+        if(!data2.articles.length){
+            const not = document.createElement('div')
+            not.className='p-5 text-center'
+            not.textContent = 'NOT FOUND'
+            row3.appendChild(not)
+        }else{
+            data2.articles.map((Element) => {
+                const card2 = document.createElement('div');
+                const img2 = document.createElement('img');
+                const card1Body2 = document.createElement('div');
+                const bodyLeni2 = document.createElement('span');
+                const bodyTitle2 = document.createElement('h2');
+                const bodtText2 = document.createElement('p');
+                card2.classList.add('card2', 'd-lg-flex', 'align-items-center', 'justify-content-lg-between');
+                card1Body2.classList.add('ps-lg-5', 'p-5', 'p-sm-0')
+                img2.classList.add('p-5', 'p-sm-0');
+                bodyTitle2.classList.add('text-center', 'text-sm-start')
+                bodyLeni2.classList.add('d-block', 'text-center', 'text-sm-start','pb-3')
+                bodtText2.classList.add('text-center', 'text-sm-start')
+                img2.src = Element.urlToImage;
+                img2.alt = Element.author;
+                bodtText2.textContent = Element.description.slice(0, 100)
+                bodyTitle2.textContent = Element.title
+                bodyLeni2.textContent = Element.author;
+                card1Body2.append(bodyLeni2, bodyTitle2, bodtText2);
+                card2.append(img2, card1Body2);
+                row3.appendChild(card2)
+            })
+        }
+       
     } catch (error) {
         console.error(error);
     }
